381c5f9329a7fc8af83c684cc6382354e694baf4
yash-op7 <yashm3375@gmail.com>
2024-09-10 03:27:37 +0530

    y

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
 "use strict";
 
 
-
-const intervalId = setInterval(() => console.log('Tick'), 1000);
-// To clear the interval: clearInterval(intervalId);
-
-setTimeout(() => clearInterval(intervalId), 4100);  // Executes after 1 second
